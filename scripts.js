0f3170680d8684a233b3507b00fe614827b20f86
Brittney Epperson <beppers87@hotmail.com>
2024-04-29 17:05:05 -0500

    Added media queries in styling for mobile responsiveness

--- scripts.js
+++ scripts.js
@@ -1,9 +1,9 @@
-window.addEventListener("load", function() {
-  const button = document.getElementById('story');
-  button.addEventListener("click", showStory);
-});
+// window.addEventListener("load", function() {
+//   const button = document.getElementById('story');
+//   button.addEventListener("click", showStory);
+// });
 
 
-function showStory() {
-  console.log("It's working!");
-}
+// function showStory() {
+//   console.log("It's working!");
+// }
